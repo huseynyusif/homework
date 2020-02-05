06320641caa01103572866c7c5154da66835cda2
Huseyn Yusifli <huseynmilan@gmail.com>
2020-02-06 00:18:37 +0400

    game

--- hangmanGame/hangman.js
+++ hangmanGame/hangman.js
@@ -1,39 +1,127 @@
-let hangman = {
-  indikiSoz: 0,
-  sozler: ["italiya", "almaniya", "braziliya", "hindistan"],
-  shanslar: 12,
-  daxilEdilmisler: [],
+var hangman = {
+  sozler: [
+    {
+      sual: "almaniya",
+      hint: "Bu bayraq hansi olkeye aiddir?",
+      shekil:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1280px-Flag_of_Germany.svg.png"
+    },
+    {
+      sual: "italiya",
+      hint: "Bu bayraq hansi olkeye aiddir?",
+      shekil: "https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg"
+    },
+    {
+      sual: "kamerun",
+      hint: "Bu bayraq hansi olkeye aiddir?",
+      shekil: "https://cdn.countryflags.com/thumbs/cameroon/flag-800.png"
+    },
+    {
+      sual: "merakes",
+      hint: "Bu bayraq hansi olkeye aiddir?",
+      shekil: "https://cdn.countryflags.com/thumbs/morocco/flag-800.png"
+    }
+  ],
+  indikiSozunIndeksi: 0,
+  indikiSoz: "",
+  qalanShanslar: 0,
+  tapilmayanHerfler: [],
+  tapilanHerfler: [],
+  qalibiyyet: 0,
 
-  sozuGoster: function() {
-    let soz = this.sozler[this.indikiSoz];
-    let ekranaCixmaliTireler = "";
+  shanslariHesabla: function() {
+    let s = this.sozler[this.indikiSozunIndeksi];
+    this.qalanShanslar = s.sual.length + 5 - this.tapilmayanHerfler.length;
+  },
 
-    for (let i = 0; i < soz.length; i++) {
-      if (this.daxilEdilmisler.indexOf(soz[i]) !== -1) {
-        ekranaCixmaliTireler = ekranaCixmaliTireler + soz[i];
+  tireleriGoster: function() {
+    let s = this.sozler[this.indikiSozunIndeksi];
+
+    this.indikiSoz = "";
+    for (let i = 0; i < s.sual.length; i++) {
+      if (this.tapilanHerfler.indexOf(s.sual[i]) !== -1) {
+        this.indikiSoz += s.sual[i];
       } else {
-        ekranaCixmaliTireler = ekranaCixmaliTireler + "-";
+        this.indikiSoz += "-";
       }
     }
-    document.querySelector("#soz").innerHTML = ekranaCixmaliTireler;
+  },
+
+  consoldaGoster: function() {
+    let s = this.sozler[this.indikiSozunIndeksi];
+
+    console.log("Qalibiyetler:", this.qalibiyyet);
+    console.log("Hint:", s.hint);
+    console.log("Daxil Edilmisler:", this.tapilmayanHerfler.join(","));
+    console.log("Tapilmali Soz:", this.indikiSoz);
+  },
+
+  browserdeGoster: function() {
+    let s = this.sozler[this.indikiSozunIndeksi];
+    document.querySelector(
+      "#wins"
+    ).innerHTML = `Qalibiyyet: ${this.qalibiyyet}`;
+    document.querySelector("#hint").innerHTML = `Hint: ${s.hint}`;
+    document.querySelector(
+      "#daxil_edilmishler"
+    ).innerHTML = `Daxil edilmisler: ${this.tapilmayanHerfler.join(",")}`;
+    document.querySelector(
+      "#qalan_shanslar"
+    ).innerHTML = `Qalan shanslar: ${this.qalanShanslar}`;
+    document.querySelector("#soz").innerHTML = `Indiki soz: ${this.indikiSoz}`;
+    document.querySelector("#shekil").src = s.shekil;
+  },
+
+  sualiGoster: function() {
+    this.shanslariHesabla();
+    this.tireleriGoster();
+    this.browserdeGoster();
+    this.consoldaGoster();
+  },
+
+  guessLetter: function(letter) {
+    if (
+      this.tapilmayanHerfler.indexOf(letter) !== -1 ||
+      this.tapilanHerfler.indexOf(letter) !== -1
+    ) {
+      return false;
+    } else {
+      let s = this.sozler[this.indikiSozunIndeksi];
+
+      if (s.sual.indexOf(letter) !== -1) {
+        this.tapilanHerfler.push(letter);
+      } else {
+        this.tapilmayanHerfler.push(letter);
+      }
+    }
+  },
+  isGameFinished: function() {
+    if (this.qalanShanslar === 0) {
+      this.resetGame();
+    } else {
+      let s = this.sozler[this.indikiSozunIndeksi];
+      let a = new Set(s.sual.split(""));
+
+      if (a.size === this.tapilanHerfler.length) {
+        this.resetGame();
+        this.qalibiyyet++;
+      }
+    }
+  },
+
+  resetGame: function() {
+    this.indikiSozunIndeksi++;
+    if (this.sozler.length <= this.indikiSozunIndeksi) {
+      this.indikiSozunIndeksi = 0;
+    }
+    this.tapilanHerfler = [];
+    this.tapilmayanHerfler = [];
   }
 };
+hangman.sualiGoster();
 
-window.onkeyup = e => {
-  hangman.sozuGoster();
-  // if (e.key === "abcdefghijklmnopqrstuvwxyz") {
-  //   if (e.key === hangman.sozler[hangman.indikiSoz]) {
-  //     document.querySelector("#soz").innerHTML = ekranaCixmaliTireler;
-  // }
-  //    else {
-  //     document.querySelector("#soz").innerHTML = ekranaCixmaliTireler;
-  //   }
-  // }
+window.onkeyup = function(e) {
+  hangman.guessLetter(e.key);
+  hangman.isGameFinished();
+  hangman.sualiGoster();
 };
-
-hangman.sozuGoster();
-
-console.log(hangman.sozuGoster());
-
-document.querySelector("#daxilEdilmisHerfler").innerHTML =
-  hangman.daxilEdilmisler;
